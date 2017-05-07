fb4cb82550331c8528befc9fae948e8e2fd0c98d
Diego Fernández Barrera <bigomby@gmail.com>
2017-05-07 14:17:58 +0200

    Funcionalidad para limpiar tags de los HTML

--- tools/util.py
+++ tools/util.py
@@ -9,6 +9,7 @@ parser.add_argument("file", help="File to process")
 parser.add_argument("--doublebackslashes", help="Fix double backslashes breaklines", action="store_true")
 parser.add_argument("--title", help="Convert the first line on the title", action="store_true")
 parser.add_argument("--baselineskip", help="Remove baselineskip", action="store_true")
+parser.add_argument("--cleanhtml", help="Remove HTML tags (keep <p>...</p>)", action="store_true")
 
 args = parser.parse_args()
 
@@ -29,6 +30,12 @@ if args.baselineskip:
     regex = r"\\\\\[2\\baselineskip]"
     subst = "}\\n\\n{"
     data = re.sub(regex, subst, data, 0)
+
+if args.cleanhtml:
+    for _ in range(0, 10):
+        data = re.sub(r"<[^>]*[^p]>", "", data, 0)
+        data = re.sub(r"<p>[\s*0-9]<\/p>", "", data, re.UNICODE)
+        data = re.sub(r"^\s*$", "", data, 0, re.MULTILINE)
 
 output_file = open(args.file, 'w')
 output_file.write(data)
